ff44eb0648e242075581e341e7d009c88ce4ac0f
Samuel Bourgeat <samuel.kumar@hotmail.fr>
2023-12-22 11:20:07 +0100

    updating delta_kl.jl

--- delta_kl.jl
+++ delta_kl.jl
@@ -6,12 +6,25 @@ using StatsBase
 using Distributions
 using Plots
 using StatsFuns
+using KernelDensity
+using QuadGK
+using Statistics
 
 
 # Import and filter data
 behaviour = CSV.read("summary_all.csv", DataFrame)
 behaviour = filter(row -> row.exp_type == "operant_place" && row.shock_color == "green" && occursin("dgrp", row.genotype), behaviour)
 
+# Remove rows where frac_time_on_shocked is NA
+behaviour = dropmissing(behaviour, :frac_time_on_shocked)
+# Convert the column to Float64
+behaviour.frac_time_on_shocked = parse.(Float64, behaviour.frac_time_on_shocked)
+# Calculate the mean and standard deviation of the column
+mean_frac_time_on_shocked = mean(behaviour.frac_time_on_shocked)
+std_frac_time_on_shocked = std(behaviour.frac_time_on_shocked)
+
+# Z-score normalize the column
+behaviour.frac_time_on_shocked = (behaviour.frac_time_on_shocked .- mean_frac_time_on_shocked) ./ std_frac_time_on_shocked
 
 # Define the model
 @model function model(data)
@@ -39,29 +52,78 @@ end
 # Generate posterior predictive distributions
 posterior_predictive = Dict{String, Vector{Float64}}()
 
+
+posterior_predictive_pdf = Dict{String, Function}()
+
+
 for (genotype, chain) in models
     mu_samples = chain[:mu]
     sigma_samples = chain[:sigma]
     predictive_samples = [rand(Normal(mu, sigma)) for (mu, sigma) in zip(mu_samples, sigma_samples)]
+    predictive_samples = reshape(predictive_samples, :)
     posterior_predictive[genotype] = predictive_samples
+
+    # Estimate a KDE from the samples
+    kde_est = kde(predictive_samples)
+
+    # Store the PDF of the KDE
+    posterior_predictive_pdf[genotype] = x -> pdf(kde_est, x)
 end
+
+
+# Get the PDF for "dgrp584"
+pdf_dgrp584 = posterior_predictive_pdf["dgrp721"]
+
+# Generate a range of x values
+x_values = range(minimum(posterior_predictive["dgrp721"]), maximum(posterior_predictive["dgrp721"]), length=1000)
+
+# Calculate the PDF values for the range of x values
+pdf_values = pdf_dgrp584.(x_values)
+
+# Plot the PDF
+plot(x_values, pdf_values, title="PDF of Posterior Predictive Distribution for dgrp584", label="")
+
+
 
 # Calculate pairwise KL divergence
 kl_divergence = Dict{Tuple{String, String}, Float64}()
 
-for (g1, samples1) in posterior_predictive
-    for (g2, samples2) in posterior_predictive
-        kl = kldivergence(empirical(samples1), empirical(samples2))
+epsilon = 1e-10  # small constant to avoid taking log of zero
+
+for (g1, kde1) in posterior_predictive_pdf
+    for (g2, kde2) in posterior_predictive_pdf
+        # Define the integrand for the KL divergence with log smoothing
+        integrand = x -> kde1(x) * (log(kde1(x) + epsilon) - log(kde2(x) + epsilon))
+
+        # Calculate the range of x values where the PDFs of kde1 and kde2 are non-zero
+        x_min = min(minimum(posterior_predictive[g1]), minimum(posterior_predictive[g2]))
+        x_max = max(maximum(posterior_predictive[g1]), maximum(posterior_predictive[g2]))
+
+        # Calculate the KL divergence using numerical integration
+        kl, err = quadgk(integrand, x_min, x_max)
+
         kl_divergence[(g1, g2)] = kl
     end
 end
 
 # Convert to Matrix and plot heatmap
+
 genotypes = keys(models)
 kl_matrix = [kl_divergence[(g1, g2)] for g1 in genotypes for g2 in genotypes]
 kl_matrix = reshape(kl_matrix, length(genotypes), length(genotypes))
 #save the kl_matrix as csv
 kl_df = DataFrame(kl_matrix, collect(genotypes))
-CSV.write("kl_divergence.csv", kl_df)
+CSV.write("kl_divergence_pp.csv", kl_df)
 
 #heatmap(genotypes, genotypes, kl_df, aspect_ratio=1, color=:viridis, clim=(0, maximum(kl_matrix)))
+
+
+
+
+
+
+
+
+
+# Calculate the KL divergence between "dgrp584" and "dgrp721"
+kl_dgrp584_dgrp721 = kl_divergence[("dgrp584", "dgrp721")]
